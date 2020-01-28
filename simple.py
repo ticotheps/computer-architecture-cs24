1d8ef7274719cdb96b52b916903dcc1784e98ad9
Tico S. Thepsourinthone <ticotheps@gmail.com>
2020-01-28 11:02:04 -0500

    Adds a simple program made during lecture yesterday

--- simple.py
+++ simple.py
@@ -1,18 +1,28 @@
 import sys
 
-PRINT_BEEJ = 1
-HALT = 2
-PRINT_NUM = 3
+PRINT_BEEJ      = 1 # 0000 0001
+HALT            = 2 # 0000 0010
+PRINT_NUM       = 3
+SAVE            = 4 # Saves a value to a register
+PRINT_REGISTER  = 5
+ADD             = 6
 
 memory = [
   PRINT_BEEJ,
-  PRINT_NUM,
-  1,
-  PRINT_NUM,
-  12,
-  PRINT_BEEJ, 
-  PRINT_NUM,
-  37,
+  SAVE,
+  65,
+  2,
+  SAVE,
+  20,
+  3,
+  ADD, 
+  2,
+  3,
+  PRINT_REGISTER,
+  2,
+  PRINT_BEEJ,
+  PRINT_BEEJ,
+  PRINT_BEEJ,
   HALT
 ]
 
@@ -35,6 +45,23 @@ while running:
     
   elif command == HALT:
     running = False
+    pc += 1
+    
+  elif command == SAVE:
+    num = memory[pc + 1]
+    reg = memory[pc + 2]
+    register[reg] = num
+    pc += 3
+    
+  elif command == ADD:
+    reg_a = memory[pc + 1]
+    reg_b = memory[pc + 2]
+    register[reg_a] += register[reg_b]
+    pc += 3
+    
+  elif command == PRINT_REGISTER:
+    reg = memory[pc + 1]
+    print(register[pc])
     pc += 1
     
   else:
